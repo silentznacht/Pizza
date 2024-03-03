b5dd44b542bc642866cc80474864b1060110869f
Kevin Escoto <96765154+silentznacht@users.noreply.github.com>
2024-03-02 23:01:56 -0500

    all done, implements soon

--- Pizza/Pizza/src/main/java/com/pizza/App.java
+++ Pizza/Pizza/src/main/java/com/pizza/App.java
@@ -8,7 +8,7 @@ public class App {
     // Scanner || Input
     static Scanner userChoice = new Scanner(System.in);
 
-    // Stores all info (toppings and sizes) for user receipt 
+    // Stores all info (toppings and sizes) for user receipt
     private static HashMap<String, Double> infoSaver = new HashMap<String, Double>();
     private static List<String> selectedToppings = new ArrayList<>();
     private static List<Double> selectedToppingPrices = new ArrayList<>();
@@ -33,7 +33,7 @@ public class App {
 
     // SizeArr, name and price
     static String[] sizeArr = Sizes.sizeArr;
-    static double [] sizePrice = Sizes.sizePrice;
+    static double[] sizePrice = Sizes.sizePrice;
 
     private static final double toppingMultiplier = (double) 1.25;
 
@@ -58,6 +58,7 @@ public class App {
 
         userChoice.nextLine();
         while (!sizeLoop) {
+            System.out.println("Starting Bill Balance: " + billBalance);
             String sizeChoice = userChoice.nextLine();
             for (int i = 0; i < sizeArr.length; i++) {
                 if (sizeArr[i].toLowerCase().contains(sizeChoice.toLowerCase())) {
@@ -74,7 +75,7 @@ public class App {
                     sizeLoop = true;
                     infoSaver.put(sizeArr[i], tempBalanceS);
                     System.out.println(infoSaver);
-                    chooseToppings(billBalance);
+                    chooseToppings(billBalance, false);
                     break;
                 }
             }
@@ -85,98 +86,139 @@ public class App {
         }
     }
 
-    static void chooseToppings(double billBalance) {
+    static void chooseToppings(double billBalance, boolean extra) {
         boolean toppingLoop = false;
+        boolean sysQuestion = false;
         boolean toppingDetected = false;
         int toppingCount = 0;
-        
+
         while (!toppingLoop) {
-            System.out.println("Frank: Alright bud, you chose your pizza topping. Now choose what toppings you'd like, all up to 3");
+            System.out.println("Starting Bill Balance: " + billBalance);
+            System.out.println(
+                    "Frank: Alright bud, you chose your pizza topping. Now choose what toppings you'd like?: ");
             String toppingChoice = userChoice.nextLine();
 
-                for (int i = 0; i < toppingsNameArr.length; i++) {
-                    if (toppingsNameArr[i].toLowerCase().contains(toppingChoice)) {
-                        toppingDetected = true;
-                        toppingCount ++;
-                        billBalance += toppingsPriceArr[i];
-                        selectedToppings.add(toppingsNameArr[i]);
-                        selectedToppingPrices.add(toppingsPriceArr[i]);
+            for (int i = 0; i < toppingsNameArr.length; i++) {
+                if (toppingsNameArr[i].toLowerCase().contains(toppingChoice.toLowerCase())) {
+                    double chosenToppingPrice = toppingsPriceArr[i];
 
-                        // Debug info 
-                        System.out.println(toppingCount + ". " + "Test: " + toppingsNameArr[i]);
-                        System.out.println("Current Bill: " + billBalance);
-                        break;
-                     } 
+                    if (!extra) {
+                        billBalance += chosenToppingPrice;
+                    }
 
-                     if (toppingDetected) {
-                        chooseToppings(billBalance);
-                        toppingLoop = true;
-                        break;
-                    } 
-                    
-                    if (toppingCount > 3) {System.out.println("[EXCEEDED TOPPINGS]"); receiptGenerator(billBalance); toppingLoop = true; break;}
+                    if (extra) {
+                        double extraPrice = chosenToppingPrice + toppingMultiplier;
+                        billBalance += extraPrice;
+                    }
+
+                    toppingDetected = true;
+                    sysQuestion = true;
+                    toppingCount++;
+
+                    selectedToppings.add(toppingsNameArr[i]);
+                    selectedToppingPrices.add(toppingsPriceArr[i]);
+
+                    // Debug info
+                    System.out.println(toppingCount + ". " + "Test: " + toppingsNameArr[i]);
+                    System.out.println("Current Bill: " + billBalance);
+
+                    if (sysQuestion) {
+                        if (toppingCount == 3) {
+                            continueToppings(toppingCount, billBalance);
+                            toppingLoop = true;
+                            break;
+                        }
+                    }
+
+                    break;
                 }
 
-                if (!toppingDetected) {System.out.println("Frank: Sorry that ain't a topping we serve here bud");}
+            }
+
+            if (!toppingDetected) {
+                System.out.println("Frank: Sorry that ain't a topping we serve here bud");
+            }
+
         }
 
     }
 
-    static void printToppings(List <String> selectedToppings, List <Double> selectedToppingPrices) {
+    static void continueToppings(int toppingCount, double billBalance) {
+        System.out.println(
+                "Frank: Well bud you have ordered 3 toppings, you want some more?(Tip Any Extra Toppings Now Will Cost Extra)");
+        String userContinue = userChoice.nextLine();
+        if (userContinue.equalsIgnoreCase("yes") || userContinue.equalsIgnoreCase("y")) {
+            chooseToppings(billBalance, true);
+        }
+        if (userContinue.equalsIgnoreCase("no") || userContinue.equalsIgnoreCase("n"))
+            ;
+        {
+            receiptGenerator(billBalance);
+        }
+    }
+
+    static void printItems(List<String> selectedToppings, List<Double> selectedToppingPrices) {
+        for (Map.Entry<String, Double> entry : infoSaver.entrySet()) {
+            String key = entry.getKey();
+            Double value = entry.getValue();
+            System.out.println("{Size: " + key + ": " + value + "}");
+        }
+
         for (String topping : selectedToppings) {
             for (double price : selectedToppingPrices) {
-                System.err.println(topping + ": " + price);
+                System.err.println("{" + topping + ":" + " $" + price + "}");
+                break;
             }
         }
     }
+
     static void receiptGenerator(double billBalance) {
-        System.err.println("Method in");
-        printToppings(selectedToppings, selectedToppingPrices);
+        System.err.println("        [RECEIPT]        ");
+        printItems(selectedToppings, selectedToppingPrices);
+        System.out.println("\nTOTAL: " + billBalance);
     }
 
     static void menu(boolean menuLoop) {
 
         try {
             do {
-                System.out.println
-                (
+                System.out.println(
                         """
-                                                    [Frank's Pizza]
-    
-                                        1. Order Pizza
-                                        2. Check Our Menu
-                                        3. Leave
-    
-                        """
-                );
-    
+                                                            [Frank's Pizza]
+
+                                                1. Order Pizza
+                                                2. Check Our Menu
+                                                3. Leave
+
+                                """);
+
                 int menuChoice = userChoice.nextInt();
-    
+
                 switch (menuChoice) {
                     case 1:
                         // In progress
                         choosePizzaSize(0);
                         menuLoop = true;
                         break;
-    
+
                     case 2:
                         sizes();
                         toppings();
                         menu(false);
                         menuLoop = true;
                         break;
-    
+
                     case 3:
                         System.out.println("Frank: Okay come again! :)");
                         menuLoop = true;
                         infoSaver.clear();
                         break;
-    
+
                     default:
                         System.out.println("Frank: Sorry man, that ain't on the options list");
                         break;
                 }
-    
+
             } while (!menuLoop);
         } catch (InputMismatchException e) {
             System.out.println("Frank: Sorry man, that ain't on the options list [Tip: Remember 1 , 2 , 3]");
